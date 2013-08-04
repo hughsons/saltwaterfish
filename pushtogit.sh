07400637847a43d1fe4787dffec107df56078c01
hughsons <hughson.simon@gmail.com>
2013-08-05 01:56:53 +0530

    SaltwaterFish-08-05

--- pushtogit.sh
+++ pushtogit.sh
@@ -1,7 +1,7 @@
 git init
 git add .
 git status
-git commit -m 'SaltwaterFish-07-11'
+git commit -m 'SaltwaterFish-08-05'
 git remote rm saltfishhubnew
 git remote add saltfishhubnew  https://github.com/hughsons/saltwaterfish.git
 git push saltfishhubnew +master
